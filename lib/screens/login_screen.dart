38568b6b98e0f1cfb41a2d04616e4763cffb7664
Luis Meza <luisrj46>
2022-07-21 20:15:40 -0500

    211, validaciones de formularios

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -21,16 +22,15 @@ class LoginScreen extends StatelessWidget {
                 ],
               ),
             ),
-            const SizedBox(
-              height: 50,
-            ),
+            const SizedBox(height: 50),
             const Text(
               "Crear una nueva cuenta",
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
-            )
+            ),
+            const SizedBox(height: 50),
           ],
         ),
       )),
@@ -43,32 +43,59 @@ class _LoginForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Form(
+        autovalidateMode: AutovalidateMode.onUserInteraction,
         child: Column(
           children: [
             TextFormField(
               autocorrect: false,
               keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.deepPurple,
-                    ),
+              decoration: InputDecorations.authInputDecoration(
+                hintText: 'luchofer@gmail.com',
+                labelText: 'Email',
+                prefixIcon: Icons.alternate_email_outlined,
+              ),
+              validator: (value) {
+                String pattern =
+                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                RegExp regExp = new RegExp(pattern);
+
+                return regExp.hasMatch(value ?? '')
+                    ? null
+                    : "El correo es invalido";
+              },
+            ),
+            SizedBox(height: 30),
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: '********',
+                labelText: 'Contraseña',
+                prefixIcon: Icons.lock,
+              ),
+              validator: (value) {
+                return (value != null && value.length >= 6)
+                    ? null
+                    : "La contraseña debe ser de 6 caracteres";
+              },
+            ),
+            SizedBox(height: 30),
+            MaterialButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Colors.deepPurple,
+              child: Container(
+                child: const Text(
+                  "Ingresar",
+                  style: TextStyle(
+                    color: Colors.white,
                   ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.deepPurple,
-                      width: 2,
-                    ),
-                  ),
-                  hintText: "luchofer@gmail.com",
-                  labelText: "Email",
-                  labelStyle: TextStyle(
-                    color: Colors.grey,
-                  ),
-                  prefixIcon: Icon(
-                    Icons.alternate_email_outlined,
-                    color: Colors.deepPurple,
-                  )),
+                ),
+              ),
+              onPressed: () {},
             )
           ],
         ),
