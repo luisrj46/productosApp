969c4bd5e0e86b898cfaf0d39b0e9b56e490d1bb
Luis Meza <luisrj46>
2022-08-11 08:01:50 -0500

    220. Widget - ProductCard - Parte 2

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -17,6 +17,8 @@ class ProductCard extends StatelessWidget {
           children: const [
             _BackgroundImage(),
             _ProductDetails(),
+            Positioned(top: 0, right: 0, child: _PriceTag()),
+            Positioned(top: 0, left: 0, child: _NotAvailable()),
           ],
         ),
       ),
@@ -35,6 +37,64 @@ class ProductCard extends StatelessWidget {
           ]);
 }
 
+class _NotAvailable extends StatelessWidget {
+  const _NotAvailable({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            'Disponible',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+      width: 100,
+      height: 50,
+      alignment: Alignment.center,
+      decoration: const BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(25), bottomLeft: Radius.circular(25))),
+    );
+  }
+}
+
+class _PriceTag extends StatelessWidget {
+  const _PriceTag({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            '\$103.35',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+      width: 100,
+      height: 70,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: Colors.indigo,
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(25), bottomLeft: Radius.circular(25))),
+    );
+  }
+}
+
 class _ProductDetails extends StatelessWidget {
   const _ProductDetails({
     Key? key,
@@ -42,12 +102,35 @@ class _ProductDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.indigo,
-      width: double.infinity,
-      height: 50,
+    return Padding(
+      padding: const EdgeInsets.only(right: 50),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        decoration: _buildBoxDecoration(),
+        width: double.infinity,
+        height: 50,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'Disco duro',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
+
+  BoxDecoration _buildBoxDecoration() => const BoxDecoration(
+        color: Colors.indigo,
+        borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(25), topRight: Radius.circular(25)),
+      );
 }
 
 class _BackgroundImage extends StatelessWidget {
@@ -59,13 +142,16 @@ class _BackgroundImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(25),
-      child: Container(
-        width: double.infinity,
-        height: 400,
-        child: const FadeInImage(
-          placeholder: AssetImage('assets/jar-loading.gif'),
-          image: NetworkImage('https://via.placeholder.com/400x300/f6f6f6'),
-          fit: BoxFit.cover,
+      child: Padding(
+        padding: const EdgeInsets.only(right: 30),
+        child: Container(
+          width: double.infinity,
+          height: 400,
+          child: const FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            image: NetworkImage('https://picsum.photos/300/300'),
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     );
