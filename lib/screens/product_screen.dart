5c78168fbf3129f59ec17b0e18242cb8d4f694e8
Luis Meza <luisrj46>
2022-08-15 18:42:38 -0500

    222. Pantalla de producto - Parte 2

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,20 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
 
 class ProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Column(
-        children: [
-          Stack(
-            children: [
-              ProductImage(),
-            ],
-          )
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ProductImage(),
+                Positioned(
+                  top: 60,
+                  left: 20,
+                  child: IconButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      icon: Icon(Icons.arrow_back_ios_new)),
+                ),
+                Positioned(
+                  top: 60,
+                  right: 20,
+                  child: IconButton(
+                      onPressed: () {
+                        //TODO funciona de la camara
+                      },
+                      icon: Icon(Icons.camera_alt_outlined)),
+                )
+              ],
+            ),
+            _ProductForm(),
+            SizedBox(height: 100)
+          ],
+        ),
       ),
-    ));
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.save_alt_outlined),
+        onPressed: () {
+          //TODO: se debe guardar el producto
+        },
+      ),
+    );
   }
+}
+
+class _ProductForm extends StatelessWidget {
+  const _ProductForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        width: double.infinity,
+        decoration: _buildBoxDecoration(),
+        child: Form(
+            child: Column(
+          children: [
+            SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: 'Nombre del producto', labelText: 'Nombre'),
+            ),
+            SizedBox(height: 30),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: '\$150', labelText: 'Precio'),
+            ),
+            SizedBox(height: 30),
+            SwitchListTile.adaptive(
+              title: const Text('Disponible'),
+              value: true,
+              onChanged: (value) {
+                //TODO:guardar el valor del swichet
+              },
+            ),
+            SizedBox(height: 30),
+          ],
+        )),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.only(
+            bottomRight: Radius.circular(25),
+            bottomLeft: Radius.circular(25),
+          ),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.white.withOpacity(0.05),
+                offset: Offset(0, 5),
+                blurRadius: 5)
+          ]);
 }
